d419ee213172f8818857b32b91481a273ba896b7
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-11 11:17:43 -0600

    Refactor logic in AsteroidMaker.

--- app/services/asteroid_maker.rb
+++ app/services/asteroid_maker.rb
@@ -4,47 +4,36 @@ class AsteroidMaker
     @start = start_date
     @end = end_date
     @asteroids = []
+    @danger_day = []
   end
 
   def find_by_date
-    rocks = AsteroidService.new(@start, @end)
-    @asteroids = rocks.run
-    danger = find_most_dangerous_day
-    @asteroids[danger.first].map do |asteroid|
+    find_most_dangerous_day
+    @asteroids[@danger_day.first].map do |asteroid|
       Asteroid.new(asteroid) if asteroid["is_potentially_hazardous_asteroid"]
     end.compact
   end
 
   def find_most_dangerous_day
-    dangerous_count = @asteroids.map do |date|
+    @asteroids = AsteroidService.new(@start, @end).run
+    @danger_day = find_danger_day
+  end
+
+  def find_danger_day
+    days.zip(dangerous_count).to_h.min
+  end
+
+  def dangerous_count
+    @asteroids.map do |date|
       date[1].count do |day|
         day["is_potentially_hazardous_asteroid"] == true
       end
     end
+  end
 
-    days = @asteroids.map do |date|
+  def days
+    @asteroids.map do |date|
       date.first
-      end
-
-    hash = days.zip(dangerous_count).to_h
-    hash.min
-  end
-end
-
-class AsteroidService
-  def initialize(start_date, end_date)
-    @start = start_date
-    @end = end_date
-  end
-
-  def run
-    conn = Faraday.new("https://api.nasa.gov/neo/rest/v1/feed")
-    response = conn.get do |req|
-      req.params['start_date'] = @start
-      req.params['end_date'] = @end
-      req.params['detailed'] = "false"
-      req.params['api_key'] = ENV['api_key']
     end
-   JSON.parse(response.body)["near_earth_objects"]
   end
 end
